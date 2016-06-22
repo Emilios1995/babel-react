1b85f289b67f048afc264644764a02b60e3b9ff1
Emilio Srougo <Emilios1995@gmail.com>
2016-06-22 14:56:25 -0500

    Update transform-remove-console.js

--- transforms/transform-remove-console.js
+++ transforms/transform-remove-console.js
@@ -1,17 +1,18 @@
-
+var template = require('babel-template');
+var generate = require('babel-generator');
 module.exports = function (babel) {
   var t = babel.types;
   return {
     visitor: {
       CallExpression(path) {
         if (path.get("callee").matchesPattern("console", true)) {
-          path.node.callee = t.functionExpression(
-                t.identifier('noOps'),
-                [t.identifier('x')],
-                t.blockStatement(
-                  [t.returnStatement(t.identifier('x'))]
-                  )
-                )
+          // path.node.callee = t.functionExpression(
+          //       t.identifier('noOps'),
+          //       [t.identifier('x')],
+          //       t.blockStatement(
+          //         [t.returnStatement(t.identifier('x'))]
+          //         )
+                // )
           
           // path.replaceWith(
           //   t.callExpression(
@@ -25,11 +26,14 @@ module.exports = function (babel) {
           //     )
           //   );
           
-          // path.node.callee = template(`
-          //   (function (target) {              
-          //     return target;
-          //   })
-          // `)
+          const echo = template(`
+            (function (args) {              
+              args.forEach((m)=> (typeof m === 'function') ? m() : m)
+            })
+          `)
+          path.replaceWithSourceString(echo({
+            args: path.get("arguments")
+          }).code)
         }
       }
     }
