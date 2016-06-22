6cc8f989d2959de8239173a7cc23351d046aff5a
Emilio Srougo <Emilios1995@gmail.com>
2016-06-22 12:48:22 -0500

    Update transform-remove-console.js

--- transforms/transform-remove-console.js
+++ transforms/transform-remove-console.js
@@ -1,16 +1,14 @@
-function noOps () {
-  return null
-}
+var noOps = function() { return null }
 module.exports = function (babel) {
   var t = babel.types;
   return {
     visitor: {
       CallExpression(path) {
         if (path.get("callee").matchesPattern("console", true)) {
-//          path.node.callee = noOps;
-          path.replaceWith(
-            t.CallExpression(noOps, path.get("arguments"))
-          );
+          path.node.callee = noOps;
+          // path.replaceWith(
+          //   t.CallExpression(noOps, path.get("arguments"))
+          // );
         }
       }
     }
